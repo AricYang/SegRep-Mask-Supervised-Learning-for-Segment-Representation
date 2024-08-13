513d8eb9d2ebfae2a0932d5e4cc597b859ead7e5
AricYang <122597519+AricYang@users.noreply.github.com>
2024-08-13 23:27:15 +0900

    Update segrep_inference.py
    Add exception handlings

--- segrep_inference.py
+++ segrep_inference.py
@@ -37,7 +37,7 @@ def parse_args():
                         help='Directory to save logs')
     parser.add_argument('--device', type=str, 
                         default='cuda:0',
-                        help='cuda device you are using')
+                        help='inferencing device you are using')
     parser.add_argument('--num_workers', type=int, default=16,
                         help='Number of workers for data loading (default: 16)')
     
@@ -66,7 +66,7 @@ class Dataset(BaseDataset):
             return image, 0, filepath
         else:
             rgba_img = Image.open(self.image_dir[idx])
-            if np.asarray(rgba_img)).shape[2] < 4:
+            if (np.asarray(rgba_img)).shape[2] < 4:
                 raise ValueError(f"Expect image shape is (H, W, 4), got {np.asarray(rgba_img).shape} instead.")
             image = rgba_img.convert('RGB')
             image = self.preprocessing_image(image)
@@ -75,39 +75,6 @@ class Dataset(BaseDataset):
             image = image * mask
     
             return (image,mask), 0, filepath
-
-class Ori_TiCo(pl.LightningModule):
-    def __init__(self):
-        super().__init__()
-        resnet = torchvision.models.resnet18()
-        
-        # Truncate FC and GAP layer in the network
-        self.backbone = nn.Sequential(*list(resnet.children())[:-2])
-        self.projection_head = TiCoProjectionHead(512, 512, 128)
-        self.criterion = TiCoLoss(gather_distributed=True)
-        
-    def forward(self, x):
-        x = self.backbone(x)
-        x = x.sum([2,3])
-        x = F.normalize(x, p=2, dim=1)
-        
-        return x
-
-    def training_step(self, batch, batch_index):
-        (x0,x1) = batch[0]
-        
-        x0 = self.forward(x0)
-        z0 = self.projection_head(x0)
-        
-        x1 = self.forward(x1)
-        z1 = self.projection_head(x1)
-        
-        loss = self.criterion(z0, z1)
-        return loss
-
-    def configure_optimizers(self):
-        optimizer = flash.core.optimizers.LARS(self.parameters(), lr=1.2) # LARS for large batch (Zhu, 2022); lr = 0.3*batch size/256 (Ciga, 2022; Stacke, 2022) 
-        return optimizer
 
 
 
@@ -145,16 +112,21 @@ if __name__ == "__main__":
         T.Normalize(mean=normalize["mean"], std=normalize["std"])
     ])
 
-    image_files = sorted(glob.glob(f'{args.data_path}/*.png'))
+    # Modify it if subdirectory exists, e.g. sorted(glob.glob(f'{args.data_path}/*/*.png'))
+    image_dir = sorted(glob.glob(f'{args.data_path}/*.png'))
 
     dataset = Dataset(image_dir,
                       no_mask = args.no_mask,
                       preprocessing_image = transform_image, 
                      )
 
-    checkpoint = args.checkpoing_dir
-    
-    model = Encoder().load_from_checkpoint(checkpoint)
+    checkpoint = args.checkpoint_dir
+
+    try:
+        model = Encoder().load_from_checkpoint(checkpoint)
+    except Exception as e:
+        print(f"Error loading checkpoint: {e}")
+        exit(1)
     
     device = torch.device(args.device)
     model = model.to(device)
@@ -166,22 +138,31 @@ if __name__ == "__main__":
         num_workers=args.num_workers,
     )
 
+    # Inferecing the representations
     embeddings = []
     model.eval()
     with torch.no_grad():
-        if args.no_mask:
-            for img, _, _ in tqdm(dataloader):
-                img = img.to(device)
-                emb = model.ori_forward(img)
-                embeddings.append(emb.cpu().detach())
-        else:
-            for (img, mask), _, _ in tqdm(dataloader):
-                img = img.to(device)
-                mask = mask.to(device)
-                emb = model.segrep_forward(img,mask)
-                embeddings.append(emb.cpu().detach())
+        try:
+            if args.no_mask:
+                for img, _, _ in tqdm(dataloader):
+                    img = img.to(device)
+                    emb = model.ori_forward(img)
+                    embeddings.append(emb.cpu().detach())
+            else:
+                for (img, mask), _, _ in tqdm(dataloader):
+                    img = img.to(device)
+                    mask = mask.to(device)
+                    emb = model.segrep_forward(img,mask)
+                    embeddings.append(emb.cpu().detach())
+        except Exception as e:
+                print(f"Error processing batch: {e}")
     
         embeddings = torch.cat(embeddings, 0)
         rep = embeddings.numpy()
-        
-        joblib.dump(rep,'inferenced_reps.pkl')
+
+    # Save the inferenced representation as .pkl file
+    try:
+        joblib.dump(rep, 'inferenced_reps.pkl')
+        print("Embeddings saved successfully.")
+    except Exception as e:
+        print(f"Error saving embeddings: {e}")
